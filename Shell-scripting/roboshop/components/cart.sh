df5a2f2e6d90813608f7ac42a324e4589120e8de
Akram, khan <Akram.khan200@gmail.com>
2023-02-19 21:10:23 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Shell-scripting/roboshop/components/cart.sh

--- Shell-scripting/roboshop/components/cart.sh
+++ Shell-scripting/roboshop/components/cart.sh
@@ -1,12 +1,11 @@
 echo cart component
 
-a="abc"
+a="test"
 
-if [ "$a" == "hghhgm" ];
-
-then
-    echo "value matches"
+if [ "$a" == "perfect" ];then
+    echo "else block"
+elif [ "$a" == "test" ] ; then
+    echo "else if block"
 else
-    echo "different values"
-
+  echo "final block"
 fi
