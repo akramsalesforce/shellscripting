ac5ab479455affeb88e59f8aa3811d2a47ac72b5
Akram, khan <Akram.khan200@gmail.com>
2023-02-19 15:53:48 +0530

    my second change

--- 01 script test.sh
+++ 01 script test.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
 Echo -e "script \n started \t  boss"
+
+a=100
+b=200
+
+add=$(($a+$b))
+date=$(date +%F)
+echo "total  = $add on " + $date
+
+read -p 'enter you name : ' name
+
+echo 'your name is $name'
+echo "your name is $name"
