d3f28ff4697789b235e19ec29423c52b036cbedb
Akram, khan <Akram.khan200@gmail.com>
2023-02-20 10:05:21 +0530

    pushing common file

---
+++ Shell-scripting/roboshop/components/Common.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+statcheck(){
+# check the status of last excuted command
+if  [ $1 -eq 0 ]; then
+
+  echo -e "\e[36m Success[0m"
+
+else
+
+  echo -e "\e[36mFailure[0m"
+
+exit 2
+fi
+
+}
+
+USER_ID=$(id u)
+{
+
+if [ "$USER_ID" -ne 0 ];then
+
+echo you should run your script as sudo or root user
+
+exit 1
+
+fi
+}
+
+print(){
+
+echo -e "\n------------------------$1-----------------" &>>$LOG_FILE
+echo -e "\e[36m $1 [0m"
+
+}
+
+LOG_FILE=/tmp/roboshop.log
+
+rm -f $LOG_FILE
