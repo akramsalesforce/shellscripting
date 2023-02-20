5a3a003ba920a46607cc857e93d3041e08db424d
Akram, khan <Akram.khan200@gmail.com>
2023-02-20 11:39:54 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ Shell-scripting/roboshop/components/mongodb.sh
@@ -0,0 +1,51 @@
+source components/Common.sh
+
+print "Downloading MongoDB package"
+curl -f -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILENAME
+Stat_check $?
+
+Print "Installing Mongo DB on server"
+yum install -y mongodb-org &>>$LOG_FILENAME
+Stat_check $?
+
+Print "Updating mongod Config file"
+sed 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+Stat_check $?
+
+
+print "Download schema"
+
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG_FILENAME
+Stat_check $?
+
+cd /tmp
+
+
+print "Extracting Archive"
+
+ unzip mongodb.zip &>>$LOG_FILE
+ #&& MV /static/* . &>>$LOG_FILE
+
+statcheck $?
+
+cd mongodb-main
+
+print "uploading catalogue file"
+
+ mongo < catalogue.js &>>$LOG_FILE
+ #&& MV /static/* . &>>$LOG_FILE
+
+statcheck $?
+
+print "uploading user file"
+mongo < users.js &>>$LOG_FILE
+ #&& MV /static/* . &>>$LOG_FILE
+
+statcheck $?
+
+systemctl start mongod && systemctl enable mongod &>>$LOG_FILE
+statcheck $?
+
+
+
+
