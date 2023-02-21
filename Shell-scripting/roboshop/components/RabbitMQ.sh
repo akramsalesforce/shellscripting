822872723423787893ec48088cd33c4da45aea7b
Akram, khan <Akram.khan200@gmail.com>
2023-02-21 21:49:00 +0530

    Merge remote-tracking branch 'origin/main'

--- Shell-scripting/roboshop/components/RabbitMQ.sh
+++ Shell-scripting/roboshop/components/RabbitMQ.sh
@@ -2,15 +2,15 @@
 
 source components/Common.sh
 
-Print "Configure YUM Repos"
+print "Configure YUM Repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>${LOG_FILE}
-StatCheck $?
+statCheck $?
 
-Print "Install ErLang & RabbitMQ"
+print "Install ErLang & RabbitMQ"
 yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm rabbitmq-server -y &>>${LOG_FILE}
-StatCheck $?
+statCheck $?
 
 
 Print "Start RabbitMQ Service"
 systemctl enable rabbitmq-server &>>${LOG_FILE} && systemctl start rabbitmq-server &>>${LOG_FILE}
-StatCheck $?
+statCheck $?
