b4202abcdea90df6f37f800961cf09aa47b395ae
Akram, khan <Akram.khan200@gmail.com>
2023-02-21 17:00:08 +0530

    Merge remote-tracking branch 'origin/main'

--- Shell-scripting/roboshop/components/Mysql.sh
+++ Shell-scripting/roboshop/components/Mysql.sh
@@ -6,36 +6,35 @@ statcheck $?
 
 
 print "Installing MySql"
-yum install mysql-community-server -y &>>$LOG_FILE
+yum install mysql-community-server -y &>>${LOG_FILE}
 statcheck $?
 
 print "start MySql"
 
-systemctl enable mysqld  &>>$LOG_FILE && systemctl start mysqld  &>>$LOG_FILE
+systemctl enable mysqld  &>>$LOG_FILE && systemctl start mysqld  &>>${LOG_FILE}
 
 statcheck $?
 
-echo 'show databases' | mysql -uroot -pRoboShop@1 &>>$LOG_FILE
+echo 'show databases' | mysql -uroot -pRoboShop@1 &>>${LOG_FILE}
 if [ $? -ne 0 ]; then
 
 print "change default password"
  echo -e "SET Password for 'root'@'localhost' = PASSWORD('RoboShop@1');" >/tmp/rootpass.sql
- DEFULT_ROOT_PASSWD=$( grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+ DEFULT_ROOT_PASSWD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
- mysql --connect-expired-password -uroot -p"${DEFULT_ROOT_PASSWD}" </tmp/rootpass.sql &>>$LOG_FILE
+ mysql --connect-expired-password -uroot -p"${DEFULT_ROOT_PASSWD}" </tmp/rootpass.sql &>>${LOG_FILE}
 
 statcheck $?
 fi
 
-echo show plugins | mysql -uroot -pRoboShop@1 2>>$LOG_FILE | grep validate_password &>>$LOG_FILE
+echo show plugins | mysql -uroot -pRoboShop@1 2>>${LOG_FILE} | grep validate_password &>>${LOG_FILE}
 
 if [ $? -eq 0 ]; then
 
   print "Uninstall password validate plugins"
   echo 'uninstall plugin validate_password;' >/tmp/pass-validate.sql
 
-  mysql --connect-expired-password -uroot -pRoboShop@1 </tmp/pass-validate.sql &>>$LOG_FILE
-
+  mysql --connect-expired-password -uroot -pRoboShop@1 </tmp/pass-validate.sql &>>${LOG_FILE}
 statcheck $?
 
   fi
@@ -43,7 +42,7 @@ statcheck $?
 
 
 print "Download schema"
-curl -f -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>$LOG_FILE
+curl -f -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>${LOG_FILE}
 statcheck $?
 
 print "Extract schema"
@@ -51,7 +50,7 @@ cd /tmp  && unzip -o mysql.zip &>>$LOG_FILE
 statcheck $?
 
 print "Load  schema"
-cd mysql-main &>>$LOG_FILE && mysql -uroot -pRoboShop@1 <shipping.sql &>>$LOG_FILE
+cd mysql-main &>>$LOG_FILE && mysql -u root -pRoboShop@1 <shipping.sql &>>$LOG_FILE
 statcheck $?
 
 
