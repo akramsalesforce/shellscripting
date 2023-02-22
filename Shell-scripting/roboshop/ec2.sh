6ad2eae099efa0dd27d903b8a580e894b432df9c
Akram, khan <Akram.khan200@gmail.com>
2023-02-22 12:40:16 +0530

    Merge remote-tracking branch 'origin/main'

--- Shell-scripting/roboshop/ec2.sh
+++ Shell-scripting/roboshop/ec2.sh
@@ -12,4 +12,4 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pra
 
 echo ${AMI_ID}
 
-aws ec2 run-instance --image-id ${AMI_ID} --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=${COMPONENT}}]"
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=${COMPONENT}}]"
